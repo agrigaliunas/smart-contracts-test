7e04340606facf36843a539365d4537ee9ddad62
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2021-12-09 20:28:17 -0300

    deployed on blockchain
    https://rinkeby.etherscan.io/address/0xf5026fc42186e980063f6d9135efecabc70787fb

---
+++ contracts/1_WriteOnBlockchain.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.7.0 < 0.8.1;
+
+contract WriteOnBlockchain{
+    string text;
+
+    function Write(string calldata _text) public{
+        text = _text;
+    }
+
+    function Read() public view returns(string memory){
+        return text;
+    }
+}
